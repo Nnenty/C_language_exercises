c2e1e73964abcaab75a93f98b6fa94ed1d8545a2
Nnenty <>
2024-03-13 22:42:33 +0300

    add new file

--- rand_id.c
+++ rand_id.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define IDSIZE 10
 #define NAMESIZE 50
@@ -18,12 +19,33 @@ char *s_gets(char *str, int size);
 
 int main(void)
 {
+    bool check;
+    FILE *file;
     struct id *storage;
+    file = fopen("id_storage", "a+b");
+    storage = malloc(sizeof(struct id));
+
+    rewind(file);
+    while (fread(storage, sizeof(struct id), 1, file) == 1)
+    {
+        printf("Ваше прошлое имя %s; ваш прошлый id ", storage->name);
+        for (int i = 0; i < IDSIZE - 1; i++)
+        {
+            printf("%d", storage->id[i]);
+        }
+        putchar('\n');
+        check = true;
+    }
+    if (check == 1)
+    {
+        putchar('\n');
+    }
 
     puts("Введите ваше имя:");
     s_gets(storage->name, NAMESIZE);
     rand_id(storage);
     read(storage);
+    fwrite(storage, sizeof(struct id), 1, file);
 
     return 0;
 }
